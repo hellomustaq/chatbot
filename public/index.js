a732e43171adfd58a16752f522840217379d5b02
Mustaque <hellomstq@gmail.com>
2019-10-24 17:09:25 +0600

    updated

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ div.id = "app"
 body.appendChild(div)
 
 var js = document.createElement("script");
-js.src = "https://raw.githack.com/hellomustaq/chatbot/master/public/script-link.js";
+js.src = "https://rawcdn.githack.com/hellomustaq/chatbot/df4773a9262a22e818f3a3296530ad7b7b2aa2ea/public/script-link.js";
 js.id = "vue-app-js";
 js.defer = true;
 js.async = true;
@@ -16,7 +16,7 @@ var css = document.createElement('link');
 css.id = "vue-app-css";
 css.rel = "stylesheet";
 css.type = "text/css";
-css.href = "https://raw.githack.com/hellomustaq/chatbot/dist/css/app.3351c57d.css";
+css.href = "https://rawcdn.githack.com/hellomustaq/chatbot/df4773a9262a22e818f3a3296530ad7b7b2aa2ea/dist/css/app.7d869e75.css";
 head.appendChild(css); 
 
 
@@ -24,5 +24,5 @@ var css1 = document.createElement('link');
 css1.id = "vue-app-css1";
 css1.rel = "stylesheet";
 css1.type = "text/css";
-css1.href = "https://raw.githack.com/hellomustaq/chatbot/dist/css/chunk-vendors.e42e700a.css";
+css1.href = "https://rawcdn.githack.com/hellomustaq/chatbot/df4773a9262a22e818f3a3296530ad7b7b2aa2ea/dist/css/chunk-vendors.e42e700a.css";
 head.appendChild(css1); 

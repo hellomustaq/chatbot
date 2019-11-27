37d4e18707be310e6d5d5becb5f8978e97680c63
Mustaque <hellomstq@gmail.com>
2019-11-27 12:32:35 +0600

    updated

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ div.id = "talentAI"
 body.appendChild(div)
 
 var js = document.createElement("script");
-js.src = "https://cdn.statically.io/gh/hellomustaq/chatbot/f13f0c58/public/script-link.js";
+js.src = "https://cdn.statically.io/gh/hellomustaq/chatbot/cf82ccc3/public/script-link.js";
 js.id = "vue-app-js";
 js.defer = true;
 js.async = true;
@@ -16,5 +16,5 @@ var css = document.createElement('link');
 css.id = "vue-app-css";
 css.rel = "stylesheet";
 css.type = "text/css";
-css.href = "https://cdn.statically.io/gh/hellomustaq/chatbot/f13f0c58/dist/css/app.185c4c28.css";
+css.href = "https://cdn.statically.io/gh/hellomustaq/chatbot/cf82ccc3/dist/css/app.185c4c28.css";
 head.appendChild(css);

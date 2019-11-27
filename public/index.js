0d05e95f4767049b434b28c5ac9dc1af4a40bf83
Mustaque <hellomstq@gmail.com>
2019-11-27 12:26:27 +0600

    updated

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ div.id = "talentAI"
 body.appendChild(div)
 
 var js = document.createElement("script");
-js.src = "https://cdn.statically.io/gh/hellomustaq/chatbot/0f280967/public/script-link.js";
+js.src = "https://cdn.statically.io/gh/hellomustaq/chatbot/f13f0c58/public/script-link.js";
 js.id = "vue-app-js";
 js.defer = true;
 js.async = true;
@@ -16,13 +16,13 @@ var css = document.createElement('link');
 css.id = "vue-app-css";
 css.rel = "stylesheet";
 css.type = "text/css";
-css.href = "https://cdn.statically.io/gh/hellomustaq/chatbot/0f280967/dist/css/app.46bb87f7.css";
+css.href = "https://cdn.statically.io/gh/hellomustaq/chatbot/f13f0c58/dist/css/app.185c4c28.css";
 head.appendChild(css); 
 
 
-var css1 = document.createElement('link');
-css1.id = "vue-app-css1";
-css1.rel = "stylesheet";
-css1.type = "text/css";
-css1.href = "https://cdn.statically.io/gh/hellomustaq/chatbot/0f280967/dist/css/chunk-vendors.e42e700a.css";
-head.appendChild(css1); 
+// var css1 = document.createElement('link');
+// css1.id = "vue-app-css1";
+// css1.rel = "stylesheet";
+// css1.type = "text/css";
+// css1.href = "https://cdn.statically.io/gh/hellomustaq/chatbot/0f280967/dist/css/chunk-vendors.e42e700a.css";
+// head.appendChild(css1);
